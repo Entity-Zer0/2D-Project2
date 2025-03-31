6e708d0aca625cabaa8bfa8b02ef037ae19191d7
Nickolai Zachesky <Nickalhoas23@gmail.com>
2025-03-31 19:45:06 -0400

    Update BaseProjectile.cs

--- 2D Project-2/Assets/Scripts/Projectiles/BaseProjectile.cs
+++ 2D Project-2/Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -11,6 +11,7 @@ public class BaseProjectile : MonoBehaviour
     public float yDir = 0f;
 
     public float lifeTime = 3f;
+    public float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,11 @@ public class BaseProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         thisObject.transform.position += new Vector3(xDir, yDir, 0) * speed;
+        if(timer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
