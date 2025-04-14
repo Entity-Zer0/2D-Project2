20c2f3fab377c57dad21370fd2aa3f6278d8c021
Nickolai Zachesky <Nickalhoas23@gmail.com>
2025-04-14 19:51:23 -0400

    Player, Movement, Starting to make Inv, MovingCamera Arrows

--- 2D Project-2/Assets/Scripts/CameraMove.cs
+++ 2D Project-2/Assets/Scripts/CameraMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public GameObject thisObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,13 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            thisObject.transform.position = new Vector3(0, -57, -10);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            thisObject.transform.position = new Vector3(0, 0, -10);
+        }
     }
 }
---
+++ 2D Project-2/Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 0.5f;
+
+    public Dictionary<string, int> myInvDict = new Dictionary<string, int>();
+
+    public TextMeshProUGUI inventoryDisplay;
+
+
+    // Start is called before the first frame update
+    public void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //myInvDict.Add("Wand", 1);
+        DisplayInventory();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += Vector3.up * speed;
+            //Debug.Log("W Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += Vector3.left * speed;
+            //Debug.Log("A Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += Vector3.down * speed;
+            //Debug.Log("S Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += Vector3.right * speed;
+            //Debug.Log("D Pressed");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*if (collision.gameObject.tag == "Key")
+        {
+            Destroy(collision.gameObject);
+        }*/
+
+        if (collision.gameObject.tag == "Death")
+        {
+            Destroy(player);
+        }
+    }
+
+
+    public void DisplayInventory()
+    {
+        inventoryDisplay.text = "Inventory\n\n";
+        foreach (var item in myInvDict)
+        {
+            inventoryDisplay.text += " " + item.Key + ": " + item.Value + "\n";
+        }
+    }
+}
